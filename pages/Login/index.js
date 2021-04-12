f12765337c0ab85dc230f040bd75ee01e73a56db
andreocunha <andreoliveiracunha20@gmail.com>
2021-04-11 21:41:50 -0300

    QRGO

--- pages/Login/index.js
+++ pages/Login/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import styles from '../../styles/Login.module.css'
 
-export function Login(){
+export default function Login(){
     return(
         <div className={styles.loginContainer}>
             <main className={styles.loginMain}>
